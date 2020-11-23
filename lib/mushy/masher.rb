6e6909c2561c6f122491bb183c02e4d44adc2aaa
Darren Cauthon <darren@cauthon.com>
2020-11-23 16:48:34 -0600

    Rename.

--- lib/mushy/masher.rb
+++ lib/mushy/masher.rb
@@ -1,7 +1,7 @@
 module Mushy
   class Masher
-    def mash one, two
-      Liquid::Template.parse(one).render SymbolizedHash.new(two)
+    def mash value, data
+      Liquid::Template.parse(value).render SymbolizedHash.new(data)
     end
   end
 end
