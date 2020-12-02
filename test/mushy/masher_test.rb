b1fc0bc5b006651bac06a02720e89d0ae7f563e9
Darren Cauthon <darren@cauthon.com>
2020-12-02 14:24:02 -0600

    This scenario is meant to work just for a string.

--- test/mushy/masher_test.rb
+++ test/mushy/masher_test.rb
@@ -6,19 +6,23 @@ describe Mushy::Masher do
 
   describe "mash" do
 
-    it "should return what we give it" do
-      value = SecureRandom.uuid
-      masher.mash(value, {} ).must_equal value
-    end
+    describe "with a string" do
 
-    it "should use basic liquid templating" do
-      value = SecureRandom.uuid
-      masher.mash("{{ name }}", { "name" => value } ).must_equal value
-    end
+      it "should return what we give it" do
+        value = SecureRandom.uuid
+        masher.mash(value, {} ).must_equal value
+      end
 
-    it "should allow for symbolized hashes" do
-      value = SecureRandom.uuid
-      masher.mash("{{ name }}", { name: value } ).must_equal value
+      it "should use basic liquid templating" do
+        value = SecureRandom.uuid
+        masher.mash("{{ name }}", { "name" => value } ).must_equal value
+      end
+
+      it "should allow for symbolized hashes" do
+        value = SecureRandom.uuid
+        masher.mash("{{ name }}", { name: value } ).must_equal value
+      end
+
     end
 
   end
