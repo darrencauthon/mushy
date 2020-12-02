a4c560eff6ee3ab5edea40b6f8a2ac3278bca16d
Darren Cauthon <darren@cauthon.com>
2020-12-02 14:41:01 -0600

    Change the shape.

--- lib/mushy/masher.rb
+++ lib/mushy/masher.rb
@@ -4,15 +4,15 @@ module Mushy
 
     def mash value, data
 
-      return if value.is_a? String
-               Liquid::Template.parse(value).render SymbolizedHash.new(data)
-             elsif value.is_a? Hash
-               value.each { |k, v| value[k] = mash v, data }
-             elsif value.is_a? Array
-               value.map { |v| mash v, data }
-             else
-               value
-             end
+      if value.is_a? String
+        return Liquid::Template.parse(value).render SymbolizedHash.new(data)
+      elsif value.is_a? Hash
+        return value.each { |k, v| value[k] = mash v, data }
+      elsif value.is_a? Array
+        return value.map { |v| mash v, data }
+      else
+        return value
+      end
 
     end
 
