6e72ed3dc83d901de34f276a2847a322eacf79c3
Darren Cauthon <darren@cauthon.com>
2020-12-02 15:15:36 -0600

    Cleanup.

--- lib/mushy/masher.rb
+++ lib/mushy/masher.rb
@@ -2,18 +2,26 @@ module Mushy
 
   class Masher
 
+    attr_accessor :mash_methods
+
+    def initialize
+      self.mash_methods = 
+        {
+          String => ->(x, d) { Liquid::Template.parse(x).render SymbolizedHash.new(d) },
+          Hash   => ->(x, d) { x.each { |k, v| x[k] = mash v, d } },
+          Array  => ->(x, d) { x.map { |x| mash x, d } }
+        }
+    end
+
     def mash value, data
+      method_for(value).call value, data
+    end
 
-      if value.is_a? String
-        return Liquid::Template.parse(value).render SymbolizedHash.new(data)
-      elsif value.is_a? Hash
-        return value.each { |k, v| value[k] = mash v, data }
-      elsif value.is_a? Array
-        return value.map { |v| mash v, data }
-      else
-        return value
-      end
-
+    def method_for value
+      mash_methods
+        .select { |k, _| value.is_a? k }
+        .map    { |_, v| v }
+        .first || ->(x, _) { x }
     end
 
   end
