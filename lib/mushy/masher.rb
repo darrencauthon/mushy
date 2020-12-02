49c0b01cb6c6462e3ac404434d14c6af0b2e66b5
Darren Cauthon <darren@cauthon.com>
2020-12-02 14:37:57 -0600

    Refactor.

--- lib/mushy/masher.rb
+++ lib/mushy/masher.rb
@@ -1,12 +1,13 @@
 module Mushy
   class Masher
     def mash value, data
-      return Liquid::Template.parse(value).render SymbolizedHash.new(data) if value.is_a? String
 
-      if value.is_a? Hash
-        value = value.each { |k, v| value[k] = mash v, data }
+      if value.is_a? String
+        return Liquid::Template.parse(value).render SymbolizedHash.new(data)
+      elsif value.is_a? Hash
+        return value.each { |k, v| value[k] = mash v, data }
       elsif value.is_a? Array
-        value = value.map { |v| mash v, data }
+        return value.map { |v| mash v, data }
       end
 
       return value
