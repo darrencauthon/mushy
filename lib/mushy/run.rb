d77b093ede8653a5a459bb718013839b108ec403
Darren Cauthon <darren@cauthon.com>
2020-12-10 14:04:45 -0600

    This is not tested.

--- lib/mushy/run.rb
+++ lib/mushy/run.rb
@@ -12,7 +12,7 @@ module Mushy
       run = find_run step, workflow
       event = build_event event_data, workflow.id, run.id
       events = run_event_and_step event, step
-      events.each { |e| runner.run_event e }
+      events.each { |e| run_event e }
       run
     end
 
