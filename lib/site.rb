5032cfc276439b0f5b2b476c51f683e33d398f36
Darren Cauthon <darren@cauthon.com>
2020-12-10 16:51:34 -0600

    Add vue.

--- lib/site.rb
+++ lib/site.rb
@@ -7,9 +7,20 @@ get '/' do
 <html>
 <head>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 </head>
 <body>
-HEY
+<div id="app">
+{{message}}
+<script type="text/javascript">
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+</script>
+</div>
 </body>
 </html>
   }
