9c20867c62c668d642a10d9c1363b5395a74614c
Darren Cauthon <darren@cauthon.com>
2020-12-31 08:20:21 -0600

    Use the step ids to determine which parent steps should be executed next.

--- lib/mushy/workflow.rb
+++ lib/mushy/workflow.rb
@@ -4,8 +4,15 @@ module Mushy
     attr_accessor :id
     attr_accessor :steps
 
+    def initialize
+      self.id = SecureRandom.uuid
+      self.steps = []
+    end
+
     def steps_for event
-      []
+      steps
+        .select { |x| x.parent_steps.select { |y| y.id == event.id } }
+        .flatten
     end
   end
 
