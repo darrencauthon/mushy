bf9d9465c1d69ef3a53537b4169704e7605f44c6
Darren Cauthon <darren@cauthon.com>
2020-12-31 11:10:02 -0600

    No more need for this dependency.

--- lib/mushy/steps/event_formatter.rb
+++ lib/mushy/steps/event_formatter.rb
@@ -2,13 +2,6 @@ module Mushy
   
   class EventFormatter < Step
 
-    attr_accessor :masher
-
-    def initialize
-      super
-      self.masher = Masher.new
-    end
-
     def process event
       event
     end
