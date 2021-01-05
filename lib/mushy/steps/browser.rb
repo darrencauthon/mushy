4dc052a59a97defdaae5be0a893207b0199b0670
Darren Cauthon <darren@cauthon.com>
2021-01-05 16:12:23 -0600

    Cleanup.

--- lib/mushy/steps/browser.rb
+++ lib/mushy/steps/browser.rb
@@ -5,14 +5,19 @@ module Mushy
   class Browser < Step
 
     def process step, config
-      browser = Ferrum::Browser.new(headless: false)
+      browser = Ferrum::Browser.new
       browser.goto config[:url]
 
-      {
+      result = {
         url: browser.url,
         cookies: browser.cookies.all.map { |k, v| v.instance_variable_get('@attributes') },
         headers: browser.headers.get,
+        body: browser.body
       }
+
+      browser.quit
+
+      result
     end
 
   end
