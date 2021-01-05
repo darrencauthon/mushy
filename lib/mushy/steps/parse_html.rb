66dacd46950704385bfd352ea0c10de2356bcfd6
Darren Cauthon <darren@cauthon.com>
2021-01-05 15:37:27 -0600

    Get basic html parsing in.

--- lib/mushy/steps/parse_html.rb
+++ lib/mushy/steps/parse_html.rb
@@ -1,9 +1,32 @@
+require 'nokogiri'
+
 module Mushy
   
   class ParseHtml < Step
 
-    def process step, config
-      [{}, {}]
+    def process event, config
+
+      content = event[config[:path]]
+      doc = Nokogiri::HTML content
+
+      matches = {}
+      config[:extract].keys.each do |key|
+        extract = config[:extract][key]
+        css, value = extract.split('|')
+        value = value || './node()'
+        matches[key] = doc.css(css).map { |x| x.xpath(value).to_s }
+      end
+
+      matches[matches.keys.first]
+         .each_with_index
+         .map { |_, i| i }
+         .map do |i|
+                matches.keys.reduce(SymbolizedHash.new( { } )) do |t, key|
+                  t[key] = matches[key][i]
+                  t[key] = t[key].strip if t[key]
+                  t
+                end
+              end
     end
 
   end
--- test/mushy/steps/parse_html_test.rb
+++ test/mushy/steps/parse_html_test.rb
@@ -5,23 +5,42 @@ describe Mushy::ParseHtml do
   let(:step) { Mushy::ParseHtml.new }
 
   let(:event) do
-    {
-        content: <<DOC
+    SymbolizedHash.new( {
+      content: <<DOC
 <html>
 <head>
-    <div id="abc">123</div>
+    <div id="abc"> 123 </div>
     <div id="efg">456</div>
 </head>
 </html>
 DOC
+    } )
+  end
+
+  let(:config) do
+    {
+      path: 'content',
+      extract: {
+        id:   'div|@id',
+        text: 'div'
+      }
     }
   end
 
-  let(:config) { {} }
+  before do
+    step.config = config
+  end
 
   it "should allow css parsing" do
-    result = step.process event, config
+    result = step.execute event
+
     result.count.must_equal 2
+
+    result[0][:id].must_equal 'abc'
+    result[0][:text].must_equal '123'
+
+    result[1][:id].must_equal 'efg'
+    result[1][:text].must_equal '456'
   end
 
 end
