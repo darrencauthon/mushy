c864d14d7f43554efa90956f35b5eddfd9984dc9
Darren Cauthon <darren@cauthon.com>
2021-01-07 10:28:41 -0600

    Reformat.

--- lib/mushy/steps/filter.rb
+++ lib/mushy/steps/filter.rb
@@ -3,7 +3,9 @@ module Mushy
   class Filter < Step
 
     def process event, config
-      matches = config[:filter].select { |k, v| event[k] != config[:filter][k] }.count == 0
+      matches = config[:filter]
+                  .select { |k, v| event[k] != config[:filter][k] }
+                  .count == 0
       matches ? event : nil
     end
 
