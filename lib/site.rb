3a230dacb10c92405dd53a802c77ee9d1e6fcad1
Darren Cauthon <darren@cauthon.com>
2021-01-18 15:45:55 -0600

    Get this running inside the same app.

--- lib/site.rb
+++ lib/site.rb
@@ -2,6 +2,13 @@ require 'sinatra'
 
 require_relative 'mushy'
 
+get '/' do
+  #`pwd`
+  #File.read(File.join('public', 'index.html'))
+  #/home/pi/Desktop/mushy
+  File.read(File.join(File.dirname(__FILE__), 'public', 'index.html'))
+end
+
 get '/steps' do
   content_type :json
   {
@@ -9,4 +16,10 @@ get '/steps' do
              Mushy::Bash.new,
            ].map { |x| x.details }
   }.to_json
+end
+
+post '/run' do
+  content_type :json
+  config = JSON.parse request.body.read
+  { config: config }.to_json
 end
