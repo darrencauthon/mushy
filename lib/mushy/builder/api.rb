6dcbaba23b9b37d084ab398d72b0d33b7843554d
Darren Cauthon <darren@cauthon.com>
2021-01-27 13:29:38 -0600

    Move over the flux details.

--- lib/mushy/builder/api.rb
+++ lib/mushy/builder/api.rb
@@ -19,6 +19,34 @@ module Mushy
         [result].flatten
       end
 
+      def self.get_fluxs
+        {
+          fluxs: Mushy::Flux.all.select { |x| x.respond_to? :details }.map do |flux|
+                         details = flux.details
+                         details[:config][:incoming_split] = { type: 'text', description: 'Split an incoming event into multiple events by this key, an each event will be processed independently.' }
+                         details[:config][:outgoing_split] = { type: 'text', description: 'Split an outgoing event into multiple events by this key.' }
+                         details[:config][:merge] = { type: 'text', description: 'A comma-delimited list of fields from the event to carry through. Use * to merge all fields.' }
+                         details[:config][:group] = { type: 'text', description: 'Group events by a field, which is stored in a key. The format is group_by|group_key.' }
+                         details[:config][:limit] = { type: 'integer', description: 'Limit the number of events to this number.' }
+                         details[:config][:join] = { type: 'text', description: 'Join all of the events from this flux into one event, under this name.' }
+                         details[:config][:sort] = { type: 'text', description: 'Sort by this key.' }
+                         details[:config][:model] = { type: 'keyvalue', description: 'Reshape the outgoing events.', value: {} }
+
+                         details[:config]
+                                .select { |_, v| v[:type] == 'keyvalue' }
+                                .select { |_, v| v[:editors].nil? }
+                                .each   do |_, v|
+                                          v[:editors] = [
+                                                    { id: 'new_key', target: 'key', field: { type: 'text', value: '', default: '' } },
+                                                    { id: 'new_value', target: 'value', field: { type: 'text', value: '', default: '' } }
+                                                  ]
+                                  end
+
+                         details
+                 end
+        }
+      end
+
     end
 
   end
--- lib/site.rb
+++ lib/site.rb
@@ -8,31 +8,7 @@ end
 
 get '/fluxs' do
   content_type :json
-  {
-    fluxs: Mushy::Flux.all.select { |x| x.respond_to? :details }.map do |flux|
-                   details = flux.details
-                   details[:config][:incoming_split] = { type: 'text', description: 'Split an incoming event into multiple events by this key, an each event will be processed independently.' }
-                   details[:config][:outgoing_split] = { type: 'text', description: 'Split an outgoing event into multiple events by this key.' }
-                   details[:config][:merge] = { type: 'text', description: 'A comma-delimited list of fields from the event to carry through. Use * to merge all fields.' }
-                   details[:config][:group] = { type: 'text', description: 'Group events by a field, which is stored in a key. The format is group_by|group_key.' }
-                   details[:config][:limit] = { type: 'integer', description: 'Limit the number of events to this number.' }
-                   details[:config][:join] = { type: 'text', description: 'Join all of the events from this flux into one event, under this name.' }
-                   details[:config][:sort] = { type: 'text', description: 'Sort by this key.' }
-                   details[:config][:model] = { type: 'keyvalue', description: 'Reshape the outgoing events.', value: {} }
-
-                   details[:config]
-                          .select { |_, v| v[:type] == 'keyvalue' }
-                          .select { |_, v| v[:editors].nil? }
-                          .each   do |_, v|
-                                    v[:editors] = [
-                                                    { id: 'new_key', target: 'key', field: { type: 'text', value: '', default: '' } },
-                                                    { id: 'new_value', target: 'value', field: { type: 'text', value: '', default: '' } }
-                                                  ]
-                                  end
-
-                   details
-                 end
-  }.to_json
+  Mushy::Builder::Api.get_fluxs.to_json
 end
 
 post '/run' do
