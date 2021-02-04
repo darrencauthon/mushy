657f4b3d59888e3a75a6eec3f42b7884c2e824b4
Darren Cauthon <darren@cauthon.com>
2021-02-04 08:38:16 -0600

    Allow for a simple open.

--- lib/mushy/builder/index.rb
+++ lib/mushy/builder/index.rb
@@ -55,7 +55,7 @@ module Mushy
    var components = {
        label: {
            props: ['label', 'description', 'hide'],
-           template: '<span><label :for="id" v-if="label != \\'\\'">{{label || ' + fancyName('id') + '}} <i v-show="description && !hide">({{description}})</i></label><a href="#" v-on:click.prevent.stop="touch=true">[open]</a></span>'
+           template: '<span><label :for="id" v-if="label != \\'\\'">{{label || ' + fancyName('id') + '}} <i v-show="description && !hide">({{description}})</i></label></span>'
        },
        text:  {
            props: ['label', 'placeholder', 'disabled', 'readonly', 'value', 'description'],
@@ -63,7 +63,7 @@ module Mushy
        },
        thintext:  {
            props: ['label', 'placeholder', 'disabled', 'readonly', 'value', 'description', 'touch'],
-           template: '<div><mip-label :id="id" :label="label" :description="description" :hide="!touch"></mip-label><input type="text" :name="id" v-if="value || touch" :placeholder="placeholder" v-bind:value="value" v-on:input="$emit(\\'update:value\\', $event.target.value);" :disabled="disabled == \\'true\\'" :readonly="readonly == \\'true\\'"></div>'
+           template: '<div><mip-label :id="id" :label="label" :description="description" :hide="!touch"></mip-label><a href="#" v-on:click.prevent.stop="touch=true" v-show="!touch">[open]</a><input type="text" :name="id" v-if="value || touch" :placeholder="placeholder" v-bind:value="value" v-on:input="$emit(\\'update:value\\', $event.target.value);" :disabled="disabled == \\'true\\'" :readonly="readonly == \\'true\\'"></div>'
        },
        hide:  {
            props: ['label', 'description'],
