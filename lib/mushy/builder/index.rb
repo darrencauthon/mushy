00ca104fe5d53019dc4c234757a0bc3f55579f1f
Darren Cauthon <darren@cauthon.com>
2021-02-04 08:24:30 -0600

    Progress with an open-close option.

--- lib/mushy/builder/index.rb
+++ lib/mushy/builder/index.rb
@@ -54,8 +54,8 @@ module Mushy
 
    var components = {
        label: {
-           props: ['label', 'description'],
-           template: '<label :for="id" v-if="label != \\'\\'">{{label || ' + fancyName('id') + '}} <i v-show="description">({{description}})</i></label>'
+           props: ['label', 'description', 'hide'],
+           template: '<label :for="id" v-if="label != \\'\\'">{{label || ' + fancyName('id') + '}} <i v-show="description && !hide">({{description}})</i></label>'
        },
        text:  {
            props: ['label', 'placeholder', 'disabled', 'readonly', 'value', 'description'],
@@ -63,7 +63,7 @@ module Mushy
        },
        thintext:  {
            props: ['label', 'placeholder', 'disabled', 'readonly', 'value', 'description', 'touch'],
-           template: '<div><mip-label :id="id" :label="label" :description="description"></mip-label><input type="text" :name="id" v-if="value || touch" :placeholder="placeholder" v-bind:value="value" v-on:input="$emit(\\'update:value\\', $event.target.value);" :disabled="disabled == \\'true\\'" :readonly="readonly == \\'true\\'"><a href="#" v-on:click.prevent.stop="touch=true">[open]</a><a href="#" v-on:click.prevent.stop="touch=false">[close]</a></div>'
+           template: '<div><mip-label :id="id" :label="label" :description="description" :hide="!touch"></mip-label><input type="text" :name="id" v-if="value || touch" :placeholder="placeholder" v-bind:value="value" v-on:input="$emit(\\'update:value\\', $event.target.value);" :disabled="disabled == \\'true\\'" :readonly="readonly == \\'true\\'"><a href="#" v-on:click.prevent.stop="touch=true">[open]</a><a href="#" v-on:click.prevent.stop="touch=false">[close]</a></div>'
        },
        hide:  {
            props: ['label', 'description'],
