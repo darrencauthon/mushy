2fd3ddc338288a94ea817f5b2d018506acfb7f0e
Darren Cauthon <darren@cauthon.com>
2021-02-03 20:14:59 -0600

    Turn this link into a button.

--- lib/mushy/builder/index.rb
+++ lib/mushy/builder/index.rb
@@ -25,7 +25,7 @@ module Mushy
                 </tr>
             </table>
             <button v-if="setup.showFlux == false" v-on:click.prevent.stop="startNew({ setup: setup, configs: configs })">Start a New Flux</button>
-            <a href="#" v-if="setup.showFlux == false" v-on:click.prevent.stop="saveFlow({ setup: setup, flow: flow })">[Save This Flow]</a>
+            <button v-if="setup.showFlux == false" v-on:click.prevent.stop="saveFlow({ setup: setup, flow: flow })">Save This Flow</button>
             <button v-if="setup.showFlux" v-on:click.prevent.stop="setup.showFlux = false">&lt; Go Back To List</button>
             <div v-if="setup.showFlux">
                 <mip-heavy :data="setup"></mip-heavy>
