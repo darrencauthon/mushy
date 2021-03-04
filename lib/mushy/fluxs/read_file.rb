9bf6735401c3ba8af317c4bb1667986b79bfffdb
Darren Cauthon <darren@cauthon.com>
2021-03-04 12:05:51 -0600

    This will read a file.

---
+++ lib/mushy/fluxs/read_file.rb
@@ -0,0 +1,42 @@
+module Mushy
+
+  class ReadFile < Flux
+
+    def self.details
+      {
+        name: 'ReadFile',
+        description: 'Read a file.',
+        config: {
+          name: {
+                  description: 'The name of the file to read.',
+                  type:        'text',
+                  value:       'file.csv',
+                },
+          directory: {
+                  description: 'The directory in which to read the file. Leave blank for the current directory.',
+                  type:        'text',
+                  shrink:      true,
+                  value:       '',
+                },
+          path: {
+                  description: 'The path in the event to return the contents of the file.',
+                  type:        'text',
+                  value:       'content',
+                },
+        },
+      }
+    end
+
+    def process event, config
+      file = config[:name]
+
+      content = File.open(file).read
+
+      {
+        config[:path] => content
+      }
+    end
+
+  end
+
+end
