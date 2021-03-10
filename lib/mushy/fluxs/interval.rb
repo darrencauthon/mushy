672b20ffaabb3e86c576c45945a131b796b0fcbe
Darren Cauthon <darren@cauthon.com>
2021-03-10 16:46:02 -0600

    Return information about the time.

--- lib/mushy/fluxs/interval.rb
+++ lib/mushy/fluxs/interval.rb
@@ -47,9 +47,27 @@ module Mushy
     end
 
     def process event, config
+      now = Time.now
       {
-        time: time
-      }
+        year: nil,
+        month: nil,
+        day: nil,
+        hour: nil,
+        minute: :min,
+        second: :sec,
+        nanosecond: :nsec,
+        utc_offset: nil,
+        weekday: :wday,
+        day_of_month: :mday,
+        day_of_year: :yday,
+        string: :to_s,
+        epoch_integer: :to_i,
+        epoch_float: :to_f,
+      }.reduce({}) do |t, i|
+        method = i[1] || i[0]
+        t[i[0]] = now.send method
+        t
+      end
     end
 
   end
