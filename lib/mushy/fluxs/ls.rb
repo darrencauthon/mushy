085a88915e9c7cf5c13f765124097dbacd250116
Darren Cauthon <darren@cauthon.com>
2021-03-10 19:15:02 -0600

    More data.

--- lib/mushy/fluxs/ls.rb
+++ lib/mushy/fluxs/ls.rb
@@ -6,28 +6,38 @@ module Mushy
       {
         name: 'Ls',
         description: 'Run the "ls" command.',
-        config: {
-          directory: {
-                       description: 'The working directory in which the command will be run.',
-                       type:        'text',
-                       value:       '',
-                     },
-        },
+        config: Mushy::Bash.details[:config].tap { |c| c.delete :command },
       }
     end
 
     def process event, config
 
-      config[:command] = 'ls'
+      config[:command] = 'ls -A -l --full-time -i'
 
       result = super event, config
 
       return result unless result[:success]
 
-      result[:text].split("\n").map do |x|
-        {
-          name: x,
-        }
+      lines = result[:text].split("\n")
+      lines.shift
+
+      lines.map do |x|
+        result = {}
+        segments = x.split ' '
+
+        result[:inode] = segments.shift; x = segments.join ' '
+        result[:help]  = segments.shift; x = segments.join ' '
+
+        help_segments = result[:help].split ''
+        result[:type] = help_segments[0]
+        result[:owner] = [1, 2, 3].map { |i| help_segments[i] }.reduce('') { |t, i| t + i }
+        result[:group] = [4, 5, 6].map { |i| help_segments[i] }.reduce('') { |t, i| t + i }
+        result[:other] = [7, 8, 9].map { |i| help_segments[i] }.reduce('') { |t, i| t + i }
+        result.delete :help
+
+        result.merge segments
+         .each_with_index
+         .reduce({}) { |t, i| t[i[1]] = i[0]; t }
       end
 
     end
