1a2c82b2a69a87a757731fd2a59465179b0407bf
Darren Cauthon <darren@cauthon.com>
2021-03-12 14:01:11 -0600

    Use the code from the file writer.

--- lib/mushy/fluxs/screenshot.rb
+++ lib/mushy/fluxs/screenshot.rb
@@ -7,11 +7,10 @@ module Mushy
       details['name'] = 'Screenshot'
       details['description'] = 'Take a screenshot of the browser.'
 
-      details[:config][:path] = {
-         description: 'The path of the file to save.',
-         type:        'text',
-         value:       'picture.jpg',
-      }
+      details[:config].merge!(Mushy::WriteFile.file_saving_config.tap do |x|
+        x[x.keys.first][:value] = 'file.jpg'
+      end)
+
       details[:config][:quality] = {
          description: 'The quality of the image, a value beteen 0-100. Only applies to jpg.',
          type:        'integer',
@@ -33,8 +32,9 @@ module Mushy
       the_result  = input[:result]
       the_config  = input[:config]
 
+      file = Mushy::WriteFile.get_file_from config
       options = {
-          path:    the_config[:path],
+          path:    file,
           full:    ['true', ''].include?(the_config[:full].to_s),
           quality: (the_config[:quality].to_s == '' ? '100' : the_config[:quality]).to_i
       }
