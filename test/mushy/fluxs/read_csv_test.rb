01b6a891dbd64c843efa07b2591cbf8e7e7a2544
Darren Cauthon <darren@cauthon.com>
2021-03-16 06:19:16 -0500

    Fix some csv tests.

--- test/mushy/fluxs/read_csv_test.rb
+++ test/mushy/fluxs/read_csv_test.rb
@@ -9,11 +9,7 @@ describe Mushy::ReadCsv do
   let(:config) { {} }
 
   it "should convert the string data to a series of events" do
-    key = SecureRandom.uuid
-
-    config[:key] = key
-
-    event[key] = %{a,b,c
+    config[:data] = %{a,b,c
 d,e,f}
 
     result = flux.process event, config
@@ -22,12 +18,9 @@ d,e,f}
   end
 
   it "should allow headers" do
-    key = SecureRandom.uuid
-
-    config[:key] = key
     config[:headers] = true
 
-    event[key] = %{a,b,c
+    config[:data] = %{a,b,c
 d,e,f}
 
     result = flux.process event, config
@@ -36,17 +29,13 @@ d,e,f}
   end
 
   it "should allow a model to convert header-less data to something better" do
-    key = SecureRandom.uuid
-
-    flux.config[:key] = key
-
     flux.config[:model] = {
       first: '{{a}}',
       second: '{{b}}',
       third: '{{c}}',
     }
 
-    event[key] = %{a,b,c
+    flux.config[:data] = %{a,b,c
 d,e,f}
 
     result = flux.execute event
