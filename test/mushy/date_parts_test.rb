16afbcee509166338da8d53bfe3d1d2d76ab182a
Darren Cauthon <darren@cauthon.com>
2021-03-23 22:51:20 -0500

    Add seconds ago to date parts.

--- test/mushy/date_parts_test.rb
+++ test/mushy/date_parts_test.rb
@@ -10,6 +10,12 @@ describe Mushy::DateParts do
       result.class.must_equal Hash
     end
 
+    it "should return seconds ago" do
+      result = Mushy::DateParts.parse (Time.now - 10)
+
+      result[:seconds_ago].round.must_equal 10
+    end
+
   end
 
 end
