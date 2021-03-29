6486a50a5b25d89b6de5ab855cba054f8f05e965
Darren Cauthon <darren@cauthon.com>
2021-03-29 07:32:11 -0500

    Comment out the code.

--- lib/mushy/fluxs/special.rb
+++ lib/mushy/fluxs/special.rb
@@ -37,36 +37,36 @@ module Mushy
       event[config[:id]]
     end
 
-    def all event, config
-      self.collection.values
-    end
+    #def all event, config
+      #self.collection.values
+    #end
 
-    def delete event, config
-      self.collection.delete get_the_id(event, config)
-      event[config[:operation_performed]] = 'deleted' if config[:operation_performed]
-      event
-    end
+    #def delete event, config
+      #self.collection.delete get_the_id(event, config)
+      #event[config[:operation_performed]] = 'deleted' if config[:operation_performed]
+      #event
+    #end
 
-    def upsert event, config
-      if self.collection[get_the_id(event, config)]
-        update event, config
-      else
-        insert event, config
-      end
-    end
+    #def upsert event, config
+      #if self.collection[get_the_id(event, config)]
+        #update event, config
+      #else
+        #insert event, config
+      #end
+    #end
 
-    def update event, config
-      item = self.collection[get_the_id(event, config)]
-      event.each { |k, v| item[k] = v } if item
-      event[config[:operation_performed]] = (item ? 'updated' : 'not exist') if config[:operation_performed]
-      event
-    end
+    #def update event, config
+      #item = self.collection[get_the_id(event, config)]
+      #event.each { |k, v| item[k] = v } if item
+      #event[config[:operation_performed]] = (item ? 'updated' : 'not exist') if config[:operation_performed]
+      #event
+    #end
 
-    def insert event, config
-      self.collection[get_the_id(event, config)] = event
-      event[config[:operation_performed]] = 'inserted' if config[:operation_performed]
-      event
-    end
+    #def insert event, config
+      #self.collection[get_the_id(event, config)] = event
+      #event[config[:operation_performed]] = 'inserted' if config[:operation_performed]
+      #event
+    #end
 
   end
 
