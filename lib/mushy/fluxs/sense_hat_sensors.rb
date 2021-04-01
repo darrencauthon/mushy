25f53621150016a85d9539ec81ce1e4e36a1f437
Darren Cauthon <darren@cauthon.com>
2021-03-31 22:37:05 -0500

    Start on a sense hat sensor.

---
+++ lib/mushy/fluxs/sense_hat_sensors.rb
@@ -0,0 +1,40 @@
+module Mushy
+
+  class SenseHatSensors < Bash
+
+    def self.details
+      details = Browser.details
+      details['name'] = 'SenseHatSensors'
+      details['description'] = 'Pull values from a Sense HAT sensor.'
+
+      details[:config][:quality] = {
+         description: 'Something',
+         type:        'text',
+         shrink:      true,
+         value:       '',
+      }
+      details
+    end
+
+    def process event, config
+      commands = [
+                   'from sense_hat import SenseHat',
+                   'import json',
+                   'sense = SenseHat()',
+                   'value = json.dumps({"pressure": sense.get_pressure()})',
+                   'print(value)',
+                 ]
+
+      command = commands.map { |x| x.gsub('"', '\"')}.join(';')
+
+      command = "python -c \"#{command}\""
+
+      config[:command] = command
+
+      result = super event, config
+
+    end
+
+  end
+
+end
