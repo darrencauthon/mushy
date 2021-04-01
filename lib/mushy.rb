193a219779c8e3320459dcf148efcff264d22be1
Darren Cauthon <darren@cauthon.com>
2021-04-01 08:18:02 -0500

    Create a base class for apps that are simple python programs.

--- lib/mushy.rb
+++ lib/mushy.rb
@@ -3,9 +3,16 @@ require 'symbolized'
 
 Dir[File.dirname(__FILE__) + '/mushy/*.rb'].each { |f| require f }
 
-important_flux_files = ['bash', 'browser'].map { |x| "#{x}.rb" }
+important_flux_files = [
+                        'bash',
+                        'browser',
+                        'simple_python_program',
+                       ].map { |x| "#{x}.rb" }
+
 Dir[File.dirname(__FILE__) + '/mushy/fluxs/*.rb']
-  .sort_by { |f| important_flux_files.any? { |x| f.end_with?(x) } ? 0 : 1 }
-  .each { |f| require f }
+  .map     { |x| [x, important_flux_files.find_index(x.split("\/")[-1]) || 9999999] }
+  .sort_by { |x| x[1] }
+  .map     { |x| x[0] }
+  .each    { |f| require f }
 
 Dir[File.dirname(__FILE__) + '/mushy/builder/*.rb'].each { |f| require f }
--- lib/mushy/fluxs/sense_hat_environmental_sensors.rb
+++ lib/mushy/fluxs/sense_hat_environmental_sensors.rb
@@ -1,34 +1,5 @@
 module Mushy
 
-  class SimplePythonProgram < Bash
-
-    def self.default_config
-      Mushy::Bash.details[:config].tap do |config|
-        config.delete :command
-        config.delete :directory
-      end
-    end
-
-    def process event, config
-
-      lines = python_program(event, config)
-                .split('\n')
-                .map { |x| x.rstrip }
-                .select { |x| x && x != '' }
-                .map { |x| x.gsub('"', '\"') }
-
-      config[:command] = "python -c \"#{lines.join(';')}\""
-
-      result = super event, config
-
-      return nil unless result[:success]
-
-      JSON.parse result[:text]
-
-    end
-
-  end
-
   class SenseHatEnvironmentalSensors < SimplePythonProgram
 
     def self.details
