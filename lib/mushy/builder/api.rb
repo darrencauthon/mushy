736255797b396535aae35f307bad86fdb373d05f
Darren Cauthon <darren@cauthon.com>
2021-10-05 15:34:09 -0500

    More formatting.

--- lib/mushy/builder/api.rb
+++ lib/mushy/builder/api.rb
@@ -176,18 +176,29 @@ module Mushy
       end
 
       def self.build_documentation_from config
-        {
-          "Basic Usage" => "
-#{config[:description]}
+        basic_usage = "#{config[:description]}"
+        if config[:config]&.any?
+          basic_usage += '<table class="table is-bordered"><thead><tr><td>Field</td><td>Description</td></tr></thead>' + config[:config].reduce("") { |t, i| "#{t}<tr><td>#{i[0]}</td><td>#{i[1][:description]}</td></tr>" } + "</table>"
+        end
 
-" + '<table class="table is-bordered"><thead><tr><td>Field</td><td>Description</td></tr></thead>' + config[:config].reduce("") { |t, i| "#{t}<tr><td>#{i[0]}</td><td>#{i[1][:description]}</td></tr>" } + "</table>"
+        {
+          "Basic Usage" => basic_usage,
         }.tap do |documentation|
           if config[:examples]
             config[:examples].each do |item|
-              documentation[item[0]] = "<pre><code>#{JSON.pretty_generate(item[1][:result])}</code></pre>"
+              documentation[item[0]] = [
+                item[1][:description],
+                code_sample('Input', item[1][:input]),
+                code_sample('Result', item[1][:result]),
+              ].select { |x| x }.reduce('') { |t, i| t + i }
             end
           end
         end
+      end
+
+      def self.code_sample title, value
+        return nil unless value
+        "<div><b>#{title}</b></div><pre><code>#{JSON.pretty_generate(value)}</code></pre>"
       end
 
     end
