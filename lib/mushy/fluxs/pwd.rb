8536dfa2cfa45de0f915187fba180acc8c666bcf
Darren Cauthon <darren@cauthon.com>
2021-10-06 16:53:35 -0500

    Add an example to the pwd.

--- lib/mushy/fluxs/pwd.rb
+++ lib/mushy/fluxs/pwd.rb
@@ -7,7 +7,46 @@ module Mushy
         name: 'Pwd',
         description: 'Run the "pwd" command.',
         config: Mushy::Bash.details[:config].tap { |c| c.delete :command },
-      }
+      }.tap do |c|
+        c[:examples] = {
+          "Example" => {
+                         description: 'This will run the pwd command and return the directory information.',
+                         result: {
+                                   pwd: {
+                                     inode: "403091",
+                                     hard_links: 5,
+                                     owner: "pi",
+                                     group: "pi",
+                                     size: 4095,
+                                     date: {
+                                       year: 2020,
+                                       month: 9,
+                                       day: 5,
+                                       hour: 10,
+                                       minute: 43,
+                                       second: 36,
+                                       nanosecond: 325720074,
+                                       utc_offset: -18001,
+                                       weekday: 2,
+                                       day_of_month: 5,
+                                       day_of_year: 278,
+                                       string: "2020-10-06 11:44:37 -0500",
+                                       epoch_integer: 1633538676,
+                                       epoch_float: 1633538676.32572,
+                                       seconds_ago: 17558.38995246
+                                     },
+                                     name: "mushy",
+                                     type: "d",
+                                     owner_permission: "rwx",
+                                     group_permission: "r-x",
+                                     other_permission: "r-x",
+                                     directory: "/home/pi/Desktop",
+                                     path: "/home/pi/Desktop/mushy"
+                                 }
+                                 }
+                       }
+                       }
+      end
     end
 
     def process event, config
