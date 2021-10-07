e609debb5dc089ff63edc53843c335c04eca1a88
Darren Cauthon <darren@cauthon.com>
2021-10-07 10:00:45 -0500

    Better document this.

--- lib/mushy/fluxs/format.rb
+++ lib/mushy/fluxs/format.rb
@@ -5,8 +5,21 @@ module Mushy
     def self.details
       {
         name: 'Format',
-        description: 'Return the event passed to it. This opens the opportunity to further alter results.',
+        description: 'Return the event passed to it. This opens the opportunity to use the common fluxing to alter the event.',
         config: {},
+        examples: {
+          "Example" => {
+                         description: 'It only returns what is passed to it.',
+                         input: {
+                                  hello: 'world',
+                                },
+                         config: {
+                                 },
+                         result: {
+                                   hello: 'world',
+                                 }
+                       },
+        }
       }
     end
 
