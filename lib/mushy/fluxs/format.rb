8870c7258bfb0d0e40d4182d498f7e1c80b27027
Darren Cauthon <darren@cauthon.com>
2021-10-07 10:21:59 -0500

    Another test case.

--- lib/mushy/fluxs/format.rb
+++ lib/mushy/fluxs/format.rb
@@ -19,6 +19,32 @@ module Mushy
                                             hello: 'world',
                                           }
                                 },
+          "Changing The Event" => {
+                                    description: 'It only returns what is passed to it.',
+                                    input: {
+                                             things: [
+                                                       { name: "Elephant", type: "Mammal" },
+                                                       { name: "Alligator", type: "Reptile" },
+                                                       { name: "Giraffe", type: "Mammal" }
+                                                     ]
+                                           },
+                                    config: { outgoing_split:"things",group:"type|animal_type" },
+                                    result: [
+                                              {
+                                                animal_type: [
+                                                              {
+                                                                name: "Elephant",
+                                                                type: "Mammal"
+                                                              },
+                                                              {
+                                                                name: "Giraffe",
+                                                                type: "Mammal"
+                                                              }
+                                                            ]
+                                              },
+                                              { animal_type:[ { name:"Alligator",type:"Reptile" } ] }
+                                            ]
+                                },
         }
       }
     end
