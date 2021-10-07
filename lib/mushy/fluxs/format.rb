b1f25c4fe5b8e0dbc131754e2e083fa5fac8381f
Darren Cauthon <darren@cauthon.com>
2021-10-07 10:01:14 -0500

    Formatting.

--- lib/mushy/fluxs/format.rb
+++ lib/mushy/fluxs/format.rb
@@ -8,17 +8,17 @@ module Mushy
         description: 'Return the event passed to it. This opens the opportunity to use the common fluxing to alter the event.',
         config: {},
         examples: {
-          "Example" => {
-                         description: 'It only returns what is passed to it.',
-                         input: {
-                                  hello: 'world',
+          "Simplest Example" => {
+                                  description: 'It only returns what is passed to it.',
+                                  input: {
+                                           hello: 'world',
+                                         },
+                                  config: {
+                                          },
+                                  result: {
+                                            hello: 'world',
+                                          }
                                 },
-                         config: {
-                                 },
-                         result: {
-                                   hello: 'world',
-                                 }
-                       },
         }
       }
     end
