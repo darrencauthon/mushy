89351e05d9a68710fa8d239b134bd11e64db2332
Darren Cauthon <darren@cauthon.com>
2022-02-26 08:44:04 -0600

    Rubocop refactor.

--- lib/mushy/fluxs/cli.rb
+++ lib/mushy/fluxs/cli.rb
@@ -1,32 +1,26 @@
-module Mushy
-  
-  class Cli < Flux
-
-    def self.details
-      {
-        name: 'Cli',
-        title: 'Start a flow via command line',
-        fluxGroup: { name: 'Starters', position: 0 },
-        description: 'Accept CLI arguments from the run command.',
-        config: {
-        },
-        examples: {
-          "Calling From The Command Line" => {
-                                               description: 'Calling the CLI with command-line arguments.',
-                                               input: "mushy start file first:John last:Doe",
-                                               result: {
-                                                         "first": "John",
-                                                         "last": "Doe"
-                                                       }
-                                             },
+class Mushy::Cli < Mushy::Flux
+  def self.details
+    {
+      name: 'Cli',
+      title: 'Start a flow via command line',
+      fluxGroup: { name: 'Starters', position: 0 },
+      description: 'Accept CLI arguments from the run command.',
+      config: {
+      },
+      examples: {
+        'Calling From The Command Line' => {
+          description: 'Calling the CLI with command-line arguments.',
+          input: 'mushy start file first:John last:Doe',
+          result: {
+            first: 'John',
+            last: 'Doe'
           }
+        }
       }
-    end
-
-    def process event, config
-      event
-    end
-
+    }
   end
 
+  def process(event, _)
+    event
+  end
 end
