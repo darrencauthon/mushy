bfe285bfa2049623553f0d7f84426ab7457917fe
Darren Cauthon <darren@cauthon.com>
2022-02-26 09:03:33 -0600

    More rubocop.

--- lib/mushy/fluxs/write_json.rb
+++ lib/mushy/fluxs/write_json.rb
@@ -1,45 +1,35 @@
 require 'csv'
 
-module Mushy
-  
-  class WriteJson < Flux
-
-    def self.details
-      {
-        name: 'WriteJson',
-        title: 'Serialize as JSON',
-        description: 'Write the incoming event as JSON.',
-        fluxGroup: { name: 'JSON' },
-        config: {
-          key: {
-                description: 'The key of the outgoing field that will contain the JSON.',
-                type:        'text',
-                value:       'json',
-              },
-        },
-        examples: {
-          "Example" => {
-                         description: 'Using this Flux to convert input to a JSON string.',
-                         input: {
-                                  people: [ { name: "John" }, { name: "Jane" } ]
-                                },
-                         config: {
-                                   key: 'apple'
-                                 },
-                         result: {
-                                   apple: "{\"people\":[{\"name\":\"John\"},{\"name\":\"Jane\"}]}"
-                                 }
-                       },
-          }
+class Mushy::WriteJson < Mushy::Flux
+  def self.details
+    {
+      name: 'WriteJson',
+      title: 'Serialize as JSON',
+      description: 'Write the incoming event as JSON.',
+      fluxGroup: { name: 'JSON' },
+      config: {
+        key: {
+          description: 'The key of the outgoing field that will contain the JSON.',
+          type: 'text',
+          value: 'json'
+        }
+      },
+      examples: {
+        'Example' => {
+          description: 'Using this Flux to convert input to a JSON string.',
+          input: {
+            people: [{ name: 'John' }, { name: 'Jane' }]
+          },
+          config: { key: 'apple' },
+          result: { apple: '{"people":[{"name":"John"},{"name":"Jane"}]}' }
+        }
       }
-    end
-
-    def process event, config
-      {
-        config[:key] => event.to_json
-      }
-    end
-
+    }
   end
 
+  def process(event, config)
+    {
+      config[:key] => event.to_json
+    }
+  end
 end
