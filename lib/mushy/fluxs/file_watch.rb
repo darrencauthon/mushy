d621d88afd9eef8a6fa834b7495922d87b5c1faa
Darren Cauthon <darren@cauthon.com>
2022-02-26 08:57:52 -0600

    Rubocop cleanup.

--- lib/mushy/fluxs/file_watch.rb
+++ lib/mushy/fluxs/file_watch.rb
@@ -1,93 +1,86 @@
 require 'listen'
 
-module Mushy
-
-  class FileWatch < Flux
-
-    def self.details
-      {
-        name: 'FileWatch',
-        title: 'Start a flow when files change',
-        fluxGroup: { name: 'Starters', position: 0 },
-        description: 'Watch for file changes.',
-        config: {
-          directory: {
-                       description: 'The directory to watch, defaults to the current directory.',
-                       type:        'text',
-                       shrink:      true,
-                       value:       '',
-                     },
-          include_all_file_details: {
-                       description: 'If true, returns all details for the file. If false, just path & name are returned',
-                       type:        'boolean',
-                       shrink:      true,
-                       value:       '',
-                     }
+class Mushy::FileWatch < Mushy::Flux
+  def self.details
+    {
+      name: 'FileWatch',
+      title: 'Start a flow when files change',
+      fluxGroup: { name: 'Starters', position: 0 },
+      description: 'Watch for file changes.',
+      config: {
+        directory: {
+          description: 'The directory to watch, defaults to the current directory.',
+          type: 'text',
+          shrink: true,
+          value: ''
         },
-        examples: {
-          "Files Added" => {
-                             description: 'When a file is added, this type of result will be returned.',
-                             result: {
-                                       modified: [],
-                                       added: [ { path: '/home/pi/Desktop/mushy/bin/hey.txt', directory: '/home/pi/Desktop/mushy/bin', name: 'hey.txt' } ],
-                                       removed:[]
-                                     }
-                           },
-          "Files Removed" => {
-                               description: 'When a file is deleted, this type of result will be returned.',
-                               result: {
-                                         modified: [],
-                                         added: [],
-                                         removed:[{ path: '/home/pi/Desktop/mushy/mushy-0.15.3.gem', directory: '/home/pi/Desktop/mushy', name: 'mushy-0.15.3.gem'} ]
-                                       }
-                             },
-          "Files Modified" => {
-                                description: 'When a file is modified, this type of result will be returned.',
-                                result: {
-                                          modified: [ { path: '/home/pi/Desktop/mushy/lib/mushy/fluxs/environment.rb', directory: '/home/pi/Desktop/mushy/lib/mushy/fluxs/environment.rb', name: 'environment.rb' } ],
-                                          added: [],
-                                          removed:[]
-                                        }
-                              },
+        include_all_file_details: {
+          description: 'If true, returns all details for the file. If false, just path & name are returned',
+          type: 'boolean',
+          shrink: true,
+          value: ''
+        }
+      },
+      examples: {
+        'Files Added' => {
+          description: 'When a file is added, this type of result will be returned.',
+          result: {
+            modified: [],
+            added: [{ path: '/home/pi/Desktop/mushy/bin/hey.txt', directory: '/home/pi/Desktop/mushy/bin', name: 'hey.txt' }],
+            removed: []
+          }
+        },
+        'Files Removed' => {
+          description: 'When a file is deleted, this type of result will be returned.',
+          result: {
+            modified: [],
+            added: [],
+            removed: [{ path: '/home/pi/Desktop/mushy/mushy-0.15.3.gem', directory: '/home/pi/Desktop/mushy', name: 'mushy-0.15.3.gem'}]
+          }
+        },
+        'Files Modified' => {
+          description: 'When a file is modified, this type of result will be returned.',
+          result: {
+            modified: [{ path: '/home/pi/Desktop/mushy/lib/mushy/fluxs/environment.rb', directory: '/home/pi/Desktop/mushy/lib/mushy/fluxs/environment.rb', name: 'environment.rb' }],
+            added: [],
+            removed: []
+          }
         }
       }
-    end
-
-    def loop(&block)
-      directory = config[:directory].to_s != '' ? config[:directory] : Dir.pwd
-
-      listener = Listen.to(directory) do |modified, added, removed|
-        the_event = {
-                      modified: modified.map { |f| get_the_details_for(f) },
-                      added: added.map { |f| get_the_details_for(f) },
-                      removed: removed.map { |f| get_the_details_for(f) }
-                    }
-        block.call the_event
-      end
-
-      listener.start
-
-      sleep
-
-    end
-
-    def process event, config
-      event
-    end
-
-    def get_the_details_for(file)
-      if config[:include_all_file_details].to_s == 'true'
-        Mushy::Ls.new.process({}, { path: file })[0]
-      else
-        segments = file.split("\/")
-        {
-          path: file,
-          name: segments.pop,
-          directory: segments.join("\/")
-        }
-      end
-    end
-
+    }
   end
 
+  def loop(&block)
+    directory = config[:directory].to_s != '' ? config[:directory] : Dir.pwd
+
+    listener = Listen.to(directory) do |modified, added, removed|
+      the_event = {
+        modified: modified.map { |f| get_the_details_for(f) },
+        added: added.map { |f| get_the_details_for(f) },
+        removed: removed.map { |f| get_the_details_for(f) }
+      }
+      block.call the_event
+    end
+
+    listener.start
+
+    sleep
+  end
+
+  def process(event, _)
+    event
+  end
+
+  def get_the_details_for(file)
+    if config[:include_all_file_details].to_s == 'true'
+      Mushy::Ls.new.process({}, { path: file })[0]
+    else
+      segments = file.split("\/")
+      {
+        path: file,
+        name: segments.pop,
+        directory: segments.join("\/")
+      }
+    end
+  end
 end
