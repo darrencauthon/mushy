dabccfb8d2f2090517d8ff04ac3995c177bb5e79
Darren Cauthon <darren@cauthon.com>
2022-08-03 16:27:59 -0500

    Clean this up.

--- lib/mushy/fluxs/get.rb
+++ lib/mushy/fluxs/get.rb
@@ -1,30 +1,23 @@
 require 'faraday'
 
-module Mushy
-  
-  class Get < Flux
-
-    def process event, config
-
-      faraday = Faraday.new do |connection|
-        connection.adapter Faraday.default_adapter
-      end
-
-      headers = config[:headers] || {}
-      data = {}
-      url = config[:url]
-
-      response = faraday.get config[:url], data, headers
-
-      {
-        status: response.status,
-        url: url,
-        reason_phrase: response.reason_phrase,
-        headers: response.headers,
-        body: response.body,
-      }
+class Mushy::Get < Flux
+  def process event, config
+    faraday = Faraday.new do |connection|
+      connection.adapter Faraday.default_adapter
     end
 
-  end
+    headers = config[:headers] || {}
+    data = {}
+    url = config[:url]
 
+    response = faraday.get config[:url], data, headers
+
+    {
+      status: response.status,
+      url: url,
+      reason_phrase: response.reason_phrase,
+      headers: response.headers,
+      body: response.body
+    }
+  end
 end
