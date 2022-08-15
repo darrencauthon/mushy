061ca3848e8647e139c15f1f880cad5cbb34d7e6
Darren Cauthon <darren@cauthon.com>
2022-08-14 20:19:26 -0500

    A better example.

--- lib/mushy/fluxs/post.rb
+++ lib/mushy/fluxs/post.rb
@@ -33,35 +33,26 @@ class Mushy::Post < Mushy::Flux
         body: {
           description: 'The body of the request.',
           type: 'text',
+          shrink: true,
           value: ''
         },
       },
       examples: {
         'Successful Call' => {
-          description: 'This will open https://www.google.com and return the result.',
+          description: 'This will send a POST to https://www.google.com.',
           config: { url: 'https://www.google.com' },
           result: {
-            url: 'https://www.google.com/',
-            status: 200,
-            title: 'Google',
-            cookies: [
-              {
-                name: '1P_JAR',
-                value: '2021-10-06-12',
-                domain: '.google.com',
-                path: '/',
-                expires: 1636117150.583117,
-                size: 19,
-                httpOnly: false,
-                secure: true,
-                session: false,
-                sameSite: 'None',
-                priority: 'Medium'
-              }
-            ],
-            headers: {},
-            time: 1.486214604,
-            body: '<html itemscope="" itemtype="http://schema.org/WebPage" lang="en">...</html>'
+            status: 400,
+            url: 'https://www.google.com',
+            time: 0.12829399993643165,
+            reason_phrase: 'OK',
+            headers: {
+              'access-control-allow-origin': '*',
+              'content-type': 'application/json; charset=utf-8',
+              'content-length': '193',
+              'keep-alive': 'timeout=5'
+            },
+            body: '{}'
           }
         }
       }
