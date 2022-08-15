c04352af016b53da8f1725c8a29b4c3060a42bda
Darren Cauthon <darren@cauthon.com>
2022-08-15 07:32:44 -0500

    Delete, too.

---
+++ lib/mushy/fluxs/delete.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'faraday'
+
+#
+# A DELETE request.
+#
+class Mushy::Delete < Mushy::HttpOperation
+  def self.details
+    super.merge({
+                  examples: {
+                    'Successful Call' => {
+                      description: "This will send a #{operation.upcase} to https://www.google.com.",
+                      config: { url: 'https://www.google.com' },
+                      result: {
+                        status: 200,
+                        url: 'https://www.google.com',
+                        time: 0.12829399993643165,
+                        reason_phrase: 'OK',
+                        headers: {
+                          'access-control-allow-origin': '*',
+                          'content-type': 'application/json; charset=utf-8',
+                          'content-length': '193',
+                          'keep-alive': 'timeout=5'
+                        },
+                        body: '{}'
+                      }
+                    }
+                  }
+                })
+  end
+end
