fe4946ffd35c819b554d9f29550404da073a40ed
Darren Cauthon <darren@cauthon.com>
2022-08-15 07:23:10 -0500

    Examples will be specific.

--- lib/mushy/fluxs/http_operation.rb
+++ lib/mushy/fluxs/http_operation.rb
@@ -46,23 +46,6 @@ class Mushy::HttpOperation < Mushy::Flux
         },
       },
       examples: {
-        'Successful Call' => {
-          description: "This will send a #{operation.upcase} to https://www.google.com.",
-          config: { url: 'https://www.google.com' },
-          result: {
-            status: 400,
-            url: 'https://www.google.com',
-            time: 0.12829399993643165,
-            reason_phrase: 'OK',
-            headers: {
-              'access-control-allow-origin': '*',
-              'content-type': 'application/json; charset=utf-8',
-              'content-length': '193',
-              'keep-alive': 'timeout=5'
-            },
-            body: '{}'
-          }
-        }
       }
     }
   end
--- lib/mushy/fluxs/post.rb
+++ lib/mushy/fluxs/post.rb
@@ -6,4 +6,27 @@ require 'faraday'
 # A POST request.
 #
 class Mushy::Post < Mushy::HttpOperation
+  def self.details
+    super.merge({
+                  examples: {
+                    'Successful Call' => {
+                      description: "This will send a #{operation.upcase} to https://www.google.com.",
+                      config: { url: 'https://www.google.com' },
+                      result: {
+                        status: 400,
+                        url: 'https://www.google.com',
+                        time: 0.12829399993643165,
+                        reason_phrase: 'OK',
+                        headers: {
+                          'access-control-allow-origin': '*',
+                          'content-type': 'application/json; charset=utf-8',
+                          'content-length': '193',
+                          'keep-alive': 'timeout=5'
+                        },
+                        body: '{}'
+                      }
+                    }
+                  }
+                })
+  end
 end
